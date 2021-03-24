59694144a95174c615ca89b65877d657c07f2ef2
Paolo Viviani <paolo.vivi@gmail.com>
2021-03-24 11:15:17 +0100

    sync

--- build_kfpkg.sh
+++ build_kfpkg.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+FIRMWARE=maixpy_v0.6.2_41_g02d12688e_minimum_with_ide_support.bin
+MODEL=facedetect.kmodel
+
+rm -rf mobilenet_package mobilenet_package.kfpkg
 mkdir mobilenet_package
-cp maixpy_binaries/maixpy_v0.6.2_41_g02d12688e.bin mobilenet_package/
-cp maixpy_binaries/mobilenet.kmodel mobilenet_package/
+cp maixpy_binaries/$FIRMWARE mobilenet_package/
+cp maixpy_binaries/$MODEL mobilenet_package/
 cp flash-list.json mobilenet_package/flash-list.json
 
 zip -rj mobilenet_package.zip mobilenet_package
--- maixpy_binaries/get_tools.sh
+++ maixpy_binaries/get_tools.sh
@@ -4,10 +4,11 @@
 
 #MaixPy firmware 0.6.2
 wget http://dl.sipeed.com/fileList/MAIX/MaixPy/release/master/maixpy_v0.6.2_41_g02d12688e/maixpy_v0.6.2_41_g02d12688e.bin &
-#Alternative maixpy firmware, yet to understand exactly what it does
-wget http://dl.sipeed.com/fileList/MAIX/MaixPy/release/master/maixpy_v0.6.2_41_g02d12688e/maixpy_v0.6.2_41_g02d12688e_openmv_kmodel_v4_with_ide_support.bin &
+#Alternative maixpy firmware http://dl.sipeed.com/shareURL/MAIX/MaixPy/release/master
+wget http://dl.sipeed.com/fileList/MAIX/MaixPy/release/master/maixpy_v0.6.2_41_g02d12688e/maixpy_v0.6.2_41_g02d12688e_minimum_with_ide_support.bin &
 # Models
 wget http://dl.sipeed.com/fileList/MAIX/MaixPy/model/mnist.kmodel &
 wget http://dl.sipeed.com/fileList/MAIX/MaixPy/model/mobilenet_0x300000.kfpkg &
+wget http://dl.sipeed.com/fileList/MAIX/MaixPy/model/face_model_at_0x300000.kfpkg &
 # Busybox (not needed for vision)
 wget http://dl.sipeed.com/fileList/MAIX/K210_Linux/Firmware/Maix-linux-5.6-rc1_tcc.bin
