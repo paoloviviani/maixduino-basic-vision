6337df9fc1147b704385d8c8cdbaec9b47e8a344
Paolo Viviani <paolo.vivi@gmail.com>
2021-03-25 09:44:01 +0100

    sync

--- flash_package.sh
+++ flash_package.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-kflash -p /dev/ttyUSB0 -b 1500000 -t -B maixduino mobilenet_package.kfpkg
+if [[ "$OSTYPE" == "darwin"* ]]; then 
+PORT=/dev/cu.usbserial-694E1C86450
+else
+PORT=/dev/ttyUSB0
+fi
+
+kflash -p $PORT -b 1500000 -t -B maixduino mobilenet_package.kfpkg
